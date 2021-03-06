2b046534422b70a84e04c1dba4a5400b03461690
Lesnov Petr <lesnov,p@gmail.com>
2021-03-06 23:45:05 +0300

    closeModalESC

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,10 +5,17 @@ const {modal, closeModal,overlay, galleryRef} = refs
 
 
 closeModal.addEventListener('click', toggleModal)
+document.addEventListener('keydown', closeModalESC)
 galleryRef.addEventListener('click', getCardMove)
 
 function toggleModal(event) {
     overlay.classList.toggle('is-hidden')
+}
+
+function closeModalESC(event) {
+    if (event.code === 'Escape') {
+        toggleModal()
+    }
 }
 
 function getCardMove(event) {
